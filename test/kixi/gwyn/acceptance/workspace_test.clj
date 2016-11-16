80637bcfd78d4be67592b07794be7aac0486c15e
Eleonore9 <eleonore.mayola@ymail.com>
2016-11-16 18:08:04 +0000

    Add acceptance test

--- test/kixi/gwyn/acceptance/workspace_test.clj
+++ test/kixi/gwyn/acceptance/workspace_test.clj
@@ -6,3 +6,27 @@
             [kixi.gwyn.model :as m]
             [witan.workspace-api.protocols :as p]
             [witan.workspace-executor.core :as wex]))
+
+(def test-inputs
+  {:fire-stations-lookup {}
+   :lfb-historic-incidents {}
+   :historical-fire-risk-scores {}})
+
+(defn read-inputs [input _ schema]
+  (get test-inputs (:witan/name input)))
+
+(defn add-input-params
+  [input]
+  (assoc-in input [:witan/params :fn] (partial read-inputs input)))
+
+(deftest gwyn-workspace-test
+  (testing "The model is run on the workspace and returns the outputs expected"
+    (let [fixed-catalog (mapv #(if (= (:witan/type %) :input) (add-input-params %) %)
+                              (:catalog m/gwyn-model))
+          workspace     {:workflow  (:workflow m/gwyn-model)
+                         :catalog   fixed-catalog
+                         :contracts (p/available-fns (m/model-library))}
+          workspace'    (s/with-fn-validation (wex/build! workspace))
+          result        (apply merge (wex/run!! workspace' {}))]
+      (is result)
+      (is (:new-fire-risk-scores result)))))
