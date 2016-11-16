cf2a7c3cd05dd6280f51620970886c2553c3f9c4
Seb Bailey <sebastian@mastodonc.com>
2016-11-16 09:11:15 +0000

    WIP: setting up model namespace

---
+++ src/kixi/gwyn/model.clj
@@ -0,0 +1,30 @@
+(ns kixi.gwyn.model
+  (:require [witan.workspace-api :refer [defmodel]
+             [witan.workspace-api.protocols :as p]
+             [witan.workspace-api.utils :refer [map-fn-meta
+                                                map-model-meta]]]))
+
+(def gwyn-model-workflow
+  "Define each step of the model"
+  [])
+
+(def gwyn-model-catalog
+  "Provides metadata for each step in the model"
+  [])
+
+(defmodel gwyn-model
+  "Defines the model"
+  {:witan/name :gwyn-model
+   :witan/version "1.0.0"}
+  {:workflow gwyn-model-workflow
+   :catalog gwyn-model-catalog})
+
+(defn model-library
+  "Lists all available functions to execute each step
+   in the model"
+  []
+  (reify p/IModelLibrary
+    (available-fns [_]
+      ())
+    (available-models [_]
+      ())))
