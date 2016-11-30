e1a02cae55193478d4d38cf1b2f71f5fea968e93
Eleonore Mayola (Eleonore9) <eleonore.mayola@ymail.com>
2016-11-30 12:21:24 +0000

    matching function to improve

--- src/kixi/gwyn/utils.clj
+++ src/kixi/gwyn/utils.clj
@@ -16,7 +16,7 @@
 
 (defn remove-punctuation
   [s]
-  (str/replace s #"[-\\\/\(\)]" " "))
+  (str/replace s #"[-\\\/\(\)_]" " "))
 
 (defn remove-empty
   [coll]
@@ -30,16 +30,32 @@
        (map #(-> %
                  str/lower-case
                  remove-punctuation
-                 str/trim
-                 (str/split #" ")
-                 remove-empty))))
+                 str/trim))))
+
+(defn prepare-lfb-data
+  [coll]
+  (map #(-> % (str/split #" ") remove-empty) coll))
+
+(defn find-match
+  "Takes as an input a collection of split lfb property names
+   (ie ('bus' 'coach' 'station' 'garage')) and a string of googleapi
+   names (ie 'bus station') and look for matches between the terms of
+   the collection and the googleapi names."
+  [lfb-coll googleapi-str]
+  (keep (fn [s] (re-matches (re-pattern (str ".*" s ".*")) googleapi-str))
+        lfb-coll))
 
 (defn match-property-names
   []
-  (let [lfb-properties (prepare-properties
-                        "data/test_data/properties_lfb.csv")
+  (let [lfb-properties (-> "data/test_data/properties_lfb.csv"
+                           prepare-properties
+                           prepare-lfb-data)
         googleapi-properties (prepare-properties
                               "data/test_data/properties_googleapi.csv")]
-    ;; (map (fn [lfb] (if (some  lfb)))
-    ;;      lfb-properties)
-    ))
+    (->> lfb-properties
+         (map #(for [g googleapi-properties]
+                 (let [match (find-match % g)]
+                   (when (not-empty match)
+                     {% (first match)}))))
+         (reduce concat)
+         (keep not-empty))))
